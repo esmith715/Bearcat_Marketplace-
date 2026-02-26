fc716b0e767ddf4c8f4a7c6bfc0855cebbc3f763
camslaw <ccestridge8@gmail.com>
2026-02-26 13:34:32 -0500

    Adjust comment

--- server/main.py
+++ server/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Vite default
+    allow_origins=["http://localhost:5173"],  # vite
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
